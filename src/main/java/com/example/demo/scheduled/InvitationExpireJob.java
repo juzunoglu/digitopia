b1d6b1991208df23735b687b7107235fe86f0e19
juzunoglu <uzunoglualihan@gmail.com>
2022-11-06 20:49:46 +0300

    update read-me

--- src/main/java/com/example/demo/scheduled/InvitationExpireJob.java
+++ src/main/java/com/example/demo/scheduled/InvitationExpireJob.java
@@ -7,9 +7,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-
 @EnableScheduling
 @Service
 @Slf4j
@@ -23,13 +20,12 @@ public class InvitationExpireJob {
 
     @Scheduled(cron = "@weekly")
     @Async
-    public void task1() {
+    public void expirePendingInvitationsWeekly() {
         int result = 0 ;
-        System.out.println(Thread.currentThread().getName()+" Task 1 executed at "+ new Date());
         if (invitationResponseRepo.existsByInvitationStatus(Invitation_Status.PENDING)) {
             result = invitationResponseRepo.updateInvitationStatusByInvitationStatus(Invitation_Status.PENDING);
-            log.info("Expired invitations: {}", result);
+            log.debug("Expired invitations: {}", result);
         }
-        log.info("No invitation was in pending state, updated: {}", result);
+        log.debug("No invitation was in pending state, updated: {}", result);
     }
 }
